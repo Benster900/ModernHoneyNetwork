76b3198aad979a940247bd2dc89ab9b37d822911
Ben Bornholm <bbornholm2194@gmail.com>
2016-11-02 11:01:13 -0400

    Updated bro script

--- scripts/deploy_bro.sh
+++ scripts/deploy_bro.sh
@@ -3,37 +3,46 @@
 set -e
 set -x
 
-# Make sure script is running as root
-if [ $# -ne 2 ]
-    then
-        echo "Wrong number of arguments supplied."
-        echo "Usage: $0 <server_url> <deploy_key>."
-        exit 1
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
-# Download the bro source code
-cd /opt
-git clone --recursive git://git.bro.org/bro
-cd bro/
 
 # Check to see if the system is centos or ubuntu
 if [ -f /etc/debian_version ]; then
 	# install dependencies
-	apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev
+	apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev git -y
+
+	# Download the bro source code
+	cd /opt
+	git clone --recursive git://git.bro.org/bro
+	cd bro/
 	
 	# compile source
 	./configure
 	make
 	make install
+	
+	# Config bro scripts
+	echo "::/0" >> /usr/local/bro/etc/networks.cfg
+	echo "0.0.0.0/0" >> /usr/local/bro/etc/networks.cfg
 
 	#start cron service and add job
 	service cron start
-	echo "0-59/5 * * * * $PREFIX/bin/broctl cron" >> /etc/crontab
+	echo "0-59/5 * * * * /usr/local/bro/bin/broctl cron" >> /etc/crontab
 
 elif [ -f /etc/redhat-release ]; then
 	# install dependencies
-	yum install cmake make gcc gcc-c++ flex bison libpcap-devel openssl-devel python-devel swig zlib-devel
+	yum install cmake make gcc gcc-c++ flex bison libpcap-devel openssl-devel python-devel swig zlib-devel git -y
 	
+	# Download the bro source code
+	cd /opt
+	git clone --recursive git://git.bro.org/bro
+	cd bro/
+	
+
 	# compile source
 	./configure
 	make
